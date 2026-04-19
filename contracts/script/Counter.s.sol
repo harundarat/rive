5cfe234d28139b6222528ceb8902229898fc93fc
Harun Al Rasyid <harundarat@gmail.com>
2026-04-19 07:22:35 +0700

    feat(contracts): add multi-network config and deployer key support

--- contracts/script/Counter.s.sol
+++ contracts/script/Counter.s.sol
@@ -10,7 +10,8 @@ contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
         counter = new Counter();
 
